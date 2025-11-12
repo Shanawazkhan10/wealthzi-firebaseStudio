6852404b7c838b10e0ebb06cc79931d1abd6f937
Shanawaz Umar Khan <shanawaz.khan@nivesh.com>
2025-11-12 11:38:13 +0000

    Investment Calculator Tools AI-Powered Investment Plan
    ye dono section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Hero from '@/components/sections/hero';
 import Team from '@/components/sections/team';
 import SuccessStories from '@/components/sections/success-stories';
 import Faq from '@/components/sections/faq';
-import Calculators from '@/components/sections/calculators';
-import InvestmentRecommendations from '@/components/sections/investment-recommendations';
 import Features from '@/components/sections/features';
 
 export default function Home() {
@@ -13,8 +11,6 @@ export default function Home() {
       <Features />
       <Team />
       <SuccessStories />
-      <InvestmentRecommendations />
-      <Calculators />
       <Faq />
     </>
   );
