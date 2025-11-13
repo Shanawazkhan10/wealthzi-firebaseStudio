ea7639425e60a6d8854d266dc0b27128575cc3d6
Shanawaz Umar Khan <shanawaz.khan@nivesh.com>
2025-11-13 07:45:10 +0000

    bhai metadata proper nhi hai usko proper karde head me proper chlna chia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import Hero from '@/components/sections/hero';
 import Team from '@/components/sections/team';
 import SuccessStories from '@/components/sections/success-stories';
@@ -6,38 +5,41 @@ import Faq from '@/components/sections/faq';
 import Features from '@/components/sections/features';
 import { metaOgImageUrl } from '@/lib/application.properties';
 import { MetaTagsData } from '@/localeData/MetaTagsData';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: MetaTagsData?.home?.title,
+  keywords: MetaTagsData?.home?.keywords,
+  description: MetaTagsData?.home?.discription,
+  twitter: {
+    card: 'summary_large_image',
+    images: [metaOgImageUrl],
+    description: MetaTagsData?.home?.discription,
+    title: MetaTagsData?.home?.title,
+    site: '@Wealthzi_in',
+    creator: '@Wealthzi_in',
+  },
+  openGraph: {
+    locale: 'en_US',
+    type: 'website',
+    title: MetaTagsData?.home?.title,
+    images: [metaOgImageUrl],
+    description: MetaTagsData?.home?.discription,
+    url: 'https://www.wealthzi.com/',
+    siteName: 'Wealthzi',
+  },
+  alternates: {
+    canonical: 'https://www.wealthzi.com/',
+  },
+  other: {
+    'itemProp-name': 'Wealthzi',
+    'itemProp-image': metaOgImageUrl,
+  },
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>{MetaTagsData?.home?.title}</title>
-        <meta name="keywords" content={MetaTagsData?.home?.keywords}></meta>
-        <meta name="description" content={MetaTagsData?.home?.discription} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content={metaOgImageUrl} />
-        <meta
-          name="twitter:description"
-          content={MetaTagsData?.home?.discription}
-        />
-        <meta name="twitter:title" content={MetaTagsData?.home?.title} />
-        <meta name="twitter:site" content="@Wealthzi_in" />
-        <meta name="twitter:creator" content="@Wealthzi_in" />
-        <meta itemProp="name" content="Wealthzi" />
-        <meta itemProp="image" content={metaOgImageUrl} />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={MetaTagsData?.home?.title} />
-        <meta property="og:image" content={metaOgImageUrl} />
-        <meta
-          property="og:description"
-          content={MetaTagsData?.home?.discription}
-        />
-        <meta property="og:url" content="https://www.wealthzi.com/" />
-        <meta property="og:site_name" content="Wealthzi" />
-        <link rel="canonical" href="https://www.wealthzi.com/" />
-      </Head>
-
       <Hero />
       <Features />
       <Team />
